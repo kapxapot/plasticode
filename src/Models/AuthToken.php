782a0936de3024b77e2e512c3f3ff9206727084b
kapxapot <kapxapot@gmail.com>
2019-06-03 22:20:12 +0300

    Added AuthToken->toString method

--- src/Models/AuthToken.php
+++ src/Models/AuthToken.php
@@ -4,19 +4,20 @@ namespace Plasticode\Models;
 
 class AuthToken extends DbModel
 {
-    // GETTERS - ONE
-    
     public static function getByToken($token)
     {
         return self::query()
             ->where('token', $token)
             ->one();
     }
-
-    // PROPS
     
     public function user()
     {
         return self::getUser($this->userId);
+    }
+
+    public function toString()
+    {
+        return $this->token . ', expires at ' . $this->expiresAt;
     }
 }
