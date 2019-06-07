42371a7102fc664864fd78c241959a8149c6cd72
Sergei Atroshchenko <sergei.atroshchenko@cbsi.com>
2019-06-07 12:18:06 +0300

    Invalid index warning fix

--- src/Core/Session.php
+++ src/Core/Session.php
@@ -4,37 +4,37 @@ namespace Plasticode\Core;
 
 class Session
 {
-	private $name;
+    private $name;
 
-	public function __construct($name)
-	{
-		$this->name = $name;
+    public function __construct($name)
+    {
+        $this->name = $name;
 
-		if (!isset($_SESSION[$this->name])) {
-			$_SESSION[$this->name] = [];
-		}
-	}
-	
-	public function get($key)
-	{
-		return $_SESSION[$this->name][$key];
-	}
+        if (!isset($_SESSION[$this->name])) {
+            $_SESSION[$this->name] = [];
+        }
+    }
+    
+    public function get($key)
+    {
+        return $_SESSION[$this->name][$key] ?? null;
+    }
 
-	public function set($key, $value)
-	{
-		$_SESSION[$this->name][$key] = $value;
-	}
-	
-	public function delete($key)
-	{
-		unset($_SESSION[$this->name][$key]);
-	}
-	
-	public function getAndDelete($key)
-	{
-		$value = $this->get($key);
-		$this->delete($key);
-		
-		return $value;
-	}
+    public function set($key, $value)
+    {
+        $_SESSION[$this->name][$key] = $value;
+    }
+    
+    public function delete($key)
+    {
+        unset($_SESSION[$this->name][$key]);
+    }
+    
+    public function getAndDelete($key)
+    {
+        $value = $this->get($key);
+        $this->delete($key);
+        
+        return $value;
+    }
 }
