fd642e7e884a334ed4edd4eac6894638bc4191c1
kapxapot <kapxapot@gmail.com>
2019-06-11 00:50:23 +0300

    Added JSON_UNESCAPED_UNICODE to Core::json()

--- src/Core/Core.php
+++ src/Core/Core.php
@@ -24,94 +24,94 @@ class Core
         Model::init($container);
     }
     
-	public static function json($response, $data, $options = [])
-	{
-	    if ($data instanceof Collection) {
-	        $data = $data->toArray();
-	    }
-	    elseif ($data instanceof \ORM) {
-	        $data = $data->asArray();
-	    }
-	    
-	    if (!is_array($data)) {
-	        throw new \InvalidArgumentException('Core::json expects an array, a Collection or a dbObj.');
-	    }
+    public static function json($response, $data, $options = [])
+    {
+        if ($data instanceof Collection) {
+            $data = $data->toArray();
+        }
+        elseif ($data instanceof \ORM) {
+            $data = $data->asArray();
+        }
+        
+        if (!is_array($data)) {
+            throw new \InvalidArgumentException('Core::json expects an array, a Collection or a dbObj.');
+        }
 
-		if (isset($options['params']['format'])) {
-			$format = $options['params']['format'];
-			
-			// datatables
-			if ($format == 'dt') {
-				$wrapper = new \stdClass;
-				$wrapper->data = $data;
-				
-				$data = $wrapper;
-			}
-		}
+        if (isset($options['params']['format'])) {
+            $format = $options['params']['format'];
+            
+            // datatables
+            if ($format == 'dt') {
+                $wrapper = new \stdClass;
+                $wrapper->data = $data;
+                
+                $data = $wrapper;
+            }
+        }
 
-		return $response->withJson($data);
-	}
+        return $response->withJson($data, null, JSON_UNESCAPED_UNICODE);
+    }
 
-	/**
-	 * Error, error!
-	 * 
-	 * @param ContainerInterface $c Slim container
-	 * @param object $response
-	 * @param \Exception $ex
-	 */
-	public static function error($c, $response, $ex, $debug = false)
-	{
-		$status = 500;
+    /**
+     * Error, error!
+     * 
+     * @param ContainerInterface $c Slim container
+     * @param object $response
+     * @param \Exception $ex
+     */
+    public static function error($c, $response, $ex, $debug = false)
+    {
+        $status = 500;
 
-		if ($ex instanceof IApiException) {
-			$status = $ex->GetErrorCode();
-		}
-		
-		$msg = null;
-		$errors = [];
+        if ($ex instanceof IApiException) {
+            $status = $ex->GetErrorCode();
+        }
+        
+        $msg = null;
+        $errors = [];
 
-		if ($ex instanceof ValidationException) {
-			foreach ($ex->errors as $field => $error) {
-				$errors[$field] = $error;
-				
-				if (!$msg) {
-					$msg = $error[0];
-				}
-			}
-		} else {
-		    //if ($debug) {
-			    $msg = $ex->getMessage();
-		    /*} else {
-		        $msg = "Server error.";
-		    }*/
-		    
-			$msg = $c->translator->translate($msg);
-		}
-		
-		$settings = $c->get('settings');
+        if ($ex instanceof ValidationException) {
+            foreach ($ex->errors as $field => $error) {
+                $errors[$field] = $error;
+                
+                if (!$msg) {
+                    $msg = $error[0];
+                }
+            }
+        } else {
+            //if ($debug) {
+                $msg = $ex->getMessage();
+            /*} else {
+                $msg = "Server error.";
+            }*/
+            
+            $msg = $c->translator->translate($msg);
+        }
+        
+        $settings = $c->get('settings');
 
-		if ($settings['log_errors']) {
-			$c->logger->error("Error: {$msg}");
-			
-			if (!($ex instanceof ValidationException)) {
-				$lines = [];
-				
-				foreach ($ex->getTrace() as $trace) {
-					/*$args = implode(', ', $trace['args']);
-					var_dump($args);*/
-					$lines[] = "{$trace['file']} ({$trace['line']}), {$trace['class']}{$trace['type']}{$trace['function']}";
-				}
+        if ($settings['log_errors']) {
+            $c->logger->error("Error: {$msg}");
+            
+            if (!($ex instanceof ValidationException)) {
+                $lines = [];
+                
+                foreach ($ex->getTrace() as $trace) {
+                    /*$args = implode(', ', $trace['args']);
+                    var_dump($args);*/
+                    $lines[] = "{$trace['file']} ({$trace['line']}), {$trace['class']}{$trace['type']}{$trace['function']}";
+                }
 
-				$c->logger->info(Text::fromLines($lines));
-			}
-		}
+                $c->logger->info(Text::fromLines($lines));
+            }
+        }
 
-		$error = [ 'error' => true, 'message' => $msg ];
-		
-		if ($errors) {
-			$error['errors'] = $errors;
-		}
-		
-		return self::json($response, $error)->withStatus($status);
-	}
+        $error = [ 'error' => true, 'message' => $msg ];
+        
+        if ($errors) {
+            $error['errors'] = $errors;
+        }
+        
+        return self::json($response, $error)->withStatus($status);
+    }
 }
