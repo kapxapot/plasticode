5fb1f6f3f5ff91bcfa9d4ba742b3f6f9ea1e1ace
Sergei Atroshchenko <sergei.atroshchenko@cbsi.com>
2019-06-13 10:48:06 +0300

    Tabs -> spaces

--- src/Middleware/TokenAuthMiddleware.php
+++ src/Middleware/TokenAuthMiddleware.php
@@ -6,21 +6,21 @@ use Plasticode\Exceptions\AuthenticationException;
 
 class TokenAuthMiddleware extends Middleware
 {
-	public function __invoke($request, $response, $next)
-	{
+    public function __invoke($request, $response, $next)
+    {
         $tokenLine = $request->getHeaderLine('Authorization');
         $lineParts = explode(' ', $tokenLine);
         
         if (count($lineParts) < 2) {
-			throw new AuthenticationException('Invalid authorization header format. Expected "Basic <token>".');
+            throw new AuthenticationException('Invalid authorization header format. Expected "Basic <token>".');
         }
         
         $token = $lineParts[1];
 
-		if ($this->auth->validateToken($token)) {
-			$response = $next($request, $response);
-		}
+        if ($this->auth->validateToken($token)) {
+            $response = $next($request, $response);
+        }
 
-		return $response;
-	}
+        return $response;
+    }
 }
