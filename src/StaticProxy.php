610f0f9a0654eaecc9c41d3a49f7312fed4ee195
Sergei Atroshchenko <sergei.atroshchenko@cbsi.com>
2019-06-21 14:52:13 +0300

    StaticProxy clean-up

--- src/StaticProxy.php
+++ src/StaticProxy.php
@@ -8,26 +8,19 @@ class StaticProxy
     
     public function __construct($modelClass)
     {
-        $this->modelClass = $modelClass;
-    }
-    
-    protected function getModelClass()
-    {
-        $class = $this->modelClass ?? null;
-        
-        if (strlen($class) == 0) {
-            throw new \Exception('Model class not set for proxy ' . static::class);
+        if (strlen($modelClass) == 0) {
+            throw new \Exception('Model class not set.');
         }
-        
-        return $class;
+
+        $this->modelClass = $modelClass;
     }
     
     public function __call($method, $args)
     {
-        $class = self::getModelClass();
+        $class = $this->modelClass;
         
         if (!method_exists($class, $method)) {
-            throw new \Exception("Class {$class} doesn't have method {$method}.");
+            throw new \Exception('Class ' . $class . ' doesn\'t have method ' . $method . '.');
         }
         
         return $class::$method(...$args);
