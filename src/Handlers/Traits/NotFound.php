24d50c73548e2b9c87eb8d7d2e585be2aef4a5e5
Sergei Atroshchenko <sergei.atroshchenko@cbsi.com>
2019-06-25 10:36:13 +0300

    Don't show 404 page for JSON requests

--- src/Handlers/Traits/NotFound.php
+++ src/Handlers/Traits/NotFound.php
@@ -2,21 +2,28 @@
 
 namespace Plasticode\Handlers\Traits;
 
+use Plasticode\Core\Core;
+use Plasticode\Exceptions\NotFoundException;
+
 trait NotFound
 {
-	public function __invoke($request, $response)
-	{
-		$params = $this->buildParams([
-			'params' => [
-				'text' => $this->translate('Page not found or moved somewhere we don\'t know where.'),
-				'title' => $this->translate('Error 404'),
-				'no_breadcrumbs' => true,
-				'no_disqus' => 1,
-				'no_social' => 1,
-			],
-		]);
+    public function __invoke($request, $response)
+    {
+        if (Core::isJsonRequest($request)) {
+            return Core::error($this->container, $response, new NotFoundException());
+        }
 
-		return $this->view->render($response, 'main/generic.twig', $params)
-			->withStatus(404);
-	}
+        $params = $this->buildParams([
+            'params' => [
+                'text' => $this->translate('Page not found or moved somewhere we don\'t know where.'),
+                'title' => $this->translate('Error 404'),
+                'no_breadcrumbs' => true,
+                'no_disqus' => 1,
+                'no_social' => 1,
+            ],
+        ]);
+
+        return $this->view->render($response, 'main/generic.twig', $params)
+            ->withStatus(404);
+    }
 }
