ab071eaef86aeae0b27d81e7df64073901b0e2f4
Sergei Atroshchenko <sergei.atroshchenko@cbsi.com>
2019-07-03 17:43:58 +0300

    EventDispatcher meat

--- src/Events/EventDispatcher.php
+++ src/Events/EventDispatcher.php
@@ -4,5 +4,22 @@ namespace Plasticode\Events;
 
 class EventDispatcher
 {
+    private $processors = [];
 
+    public function registerProcessor(string $eventClass, string $processorClass)
+    {
+        if (!array_key_exists($eventClass, $this->processors)) {
+            $this->processors[$eventClass] = [];
+        }
+        elseif (in_array($processorClass, $this->processors[$eventClass])) {
+            return;
+        }
+
+        $this->processors[$eventClass][] = $processorClass;
+    }
+
+    public function dispatch(Event $event, Event $parentEvent = null)
+    {
+        $eventClass = 
+    }
 }
