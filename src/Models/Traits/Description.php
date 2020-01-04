22473591144d00b0f8daad8f7e186dded243c881
kapxapot <kapxapot@gmail.com>
2020-01-05 01:40:53 +0300

    Description trait fix

--- src/Models/Traits/Description.php
+++ src/Models/Traits/Description.php
@@ -2,13 +2,20 @@
 
 namespace Plasticode\Models\Traits;
 
+use Plasticode\Parsing\ParsingContext;
+
 trait Description
 {
-    public function parsedDescription() : ?string
+    public function parsedDescription() : ?ParsingContext
     {
         $context = self::$parser->parse($this->description);
+
+        if (is_null($context)) {
+            return null;
+        }
+    
         $context = self::$parser->renderLinks($context);
 
-        return $context->text;
+        return $context;
     }
 }
