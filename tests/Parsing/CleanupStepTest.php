77221aec407d7d521ade27657ef40352cc218d28
kapxapot <kapxapot@gmail.com>
2020-01-07 02:35:02 +0300

    CleanupStep tests

---
+++ tests/Parsing/CleanupStepTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Plasticode\Tests\Parsing;
+
+use Plasticode\Parsing\Interfaces\ParsingStepInterface;
+use Plasticode\Parsing\Steps\CleanupStep;
+
+final class CleanupStepTest extends ParsingTestCase
+{
+    /** @var \Plasticode\Parsing\Steps\CleanupStep */
+    private $step;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+
+        $this->step = new CleanupStep($this->config);
+    }
+
+    protected function tearDown() : void
+    {
+        unset($this->step);
+
+        parent::tearDown();
+    }
+
+    protected function step() : ParsingStepInterface
+    {
+        return $this->step;
+    }
+
+    /**
+     * @covers CleanupStep
+     */
+    public function testContextIsImmutable() : void
+    {
+        $this->assertContextIsImmutable();
+    }
+
+    /**
+     * @covers CleanupStep
+     */
+    public function testParse() : void
+    {
+        $context = $this->parseLines(
+            [
+                '<p><p><p>',
+                '</p></p></p>',
+                '<p><ul></ul></p>',
+                '<p><div></div></p>',
+                '<br/><div></div><br/>'
+            ]
+        );
+
+        $this->assertEquals(
+            [
+                '<p>',
+                '</p>',
+                '<ul></ul>',
+                '<div></div>',
+                '<div></div>'
+            ],
+            $context->getLines()
+        );
+    }
+}
