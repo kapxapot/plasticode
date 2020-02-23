79b51b228d6e542034149902b230f6b5b82f974a
kapxapot <kapxapot@gmail.com>
2020-02-23 20:34:29 +0300

    Classes::shortName() missing test

--- tests/Util/ClassesTest.php
+++ tests/Util/ClassesTest.php
@@ -8,6 +8,14 @@ use Plasticode\ViewModels\SpoilerViewModel;
 
 final class ClassesTest extends TestCase
 {
+    public function testShortName() : void
+    {
+        $this->assertEquals(
+            'ClassesTest',
+            Classes::shortName(ClassesTest::class)
+        );
+    }
+
     public function testGetPublicMethods() : void
     {
         $this->assertEquals(
