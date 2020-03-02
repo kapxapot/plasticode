4011685363d2963b33c4eaa3aff78eac1b9fc84b
Sergei Atroshchenko <sergei.atroshchenko@cbsi.com>
2020-03-02 11:58:30 +0300

    More Tags trait improvements

--- src/Repositories/Idiorm/Traits/Tags.php
+++ src/Repositories/Idiorm/Traits/Tags.php
@@ -5,24 +5,17 @@ namespace Plasticode\Repositories\Idiorm\Traits;
 use Plasticode\Query;
 use Plasticode\Repositories\Interfaces\TagRepositoryInterface;
 use Plasticode\Util\Strings;
-use Webmozart\Assert\Assert;
 
-/**
- * To use this trait you MUST declare:
- * 
- * - private TagRepositoryInterface $tagRepository
- * - public function getTable() : string (already declared in IdiormRepository class)
- * 
- * @property TagRepositoryInterface $tagRepository
- */
 trait Tags
 {
-    protected function getByTagQuery(string $tag, Query $baseQuery) : Query
+    protected function getByTagQuery(
+        TagRepositoryInterface $tagRepository,
+        Query $baseQuery,
+        string $tag
+    ) : Query
     {
-        Assert::notNull($this->tagRepository);
-
         $tag = Strings::normalize($tag);
-        $ids = $this->tagRepository->getIdsByTag($this->getTable(), $tag);
+        $ids = $tagRepository->getIdsByTag($this->getTable(), $tag);
 
         if ($ids->empty()) {
             return Query::empty();
