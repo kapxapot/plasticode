7018760ca7d311f311f37133694ad5b598560e69
kapxapot <kapxapot@gmail.com>
2020-03-27 00:41:47 +0300

    Convert::fromBit() + tests

--- src/Util/Convert.php
+++ src/Util/Convert.php
@@ -2,6 +2,8 @@
 
 namespace Plasticode\Util;
 
+use Webmozart\Assert\Assert;
+
 class Convert
 {
     /**
@@ -10,12 +12,28 @@ class Convert
      * true => 1
      * false => 0
      * null => 0
-     *
-     * @param null|bool $value
-     * @return int
      */
     public static function toBit(?bool $value) : int
     {
         return ($value === true) ? 1 : 0;
+    }
+
+    /**
+     * Converts bit value to bool.
+     *
+     * 1 => true
+     * 0 => false
+     * null => false
+     * anything else => exception
+     */
+    public static function fromBit(?int $value) : bool
+    {
+        if (is_null($value)) {
+            return false;
+        }
+
+        Assert::true(in_array($value, [0, 1]));
+
+        return $value == 1;
     }
 }
--- tests/Util/ConvertTest.php
+++ tests/Util/ConvertTest.php
@@ -17,12 +17,51 @@ final class ConvertTest extends TestCase
         $this->assertEquals($expected, $actual);
     }
 
-    public function toBitProvider()
+    public function toBitProvider() : array
     {
         return [
             [true, 1],
             [false, 0],
             [null, 0],
+        ];
+    }
+
+    /**
+     * @dataProvider fromBitProvider
+     */
+    public function testFromBit(?int $source, bool $expected) : void
+    {
+        $actual = Convert::fromBit($source);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function fromBitProvider() : array
+    {
+        return [
+            [null, false],
+            [0, false],
+            [1, true],
+        ];
+    }
+
+    /**
+     * @dataProvider fromBitExceptionProvider
+     */
+    public function testFromBitException(int $source) : void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        Convert::fromBit($source);
+    }
+
+    public function fromBitExceptionProvider() : array
+    {
+        return [
+            [-100],
+            [-1],
+            [2],
+            [100],
         ];
     }
 }
