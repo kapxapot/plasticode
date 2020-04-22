2f5c1bfd9f2ad899cba3b6d8096acc99abb9e6fd
kapxapot <kapxapot@gmail.com>
2020-04-23 00:22:39 +0300

    Removes redundant code in model published traits

--- src/Models/Traits/FullPublished.php
+++ src/Models/Traits/FullPublished.php
@@ -2,8 +2,6 @@
 
 namespace Plasticode\Models\Traits;
 
-use Plasticode\Models\DbModel;
-use Plasticode\Query;
 use Plasticode\Util\Date;
 
 /**
@@ -13,29 +11,24 @@ trait FullPublished
 {
     use Published
     {
-        Published::wherePublished as protected parentWherePublished;
         publish as protected parentPublish;
         isPublished as protected parentIsPublished;
-    }
-
-    protected static function wherePublished(Query $query) : Query
-    {
-        return self::parentWherePublished($query)
-            ->whereRaw('(published_at < now())');
     }
 
     public function publish()
     {
         $this->parentPublish();
 
-        if ($this->publishedAt === null) {
+        if (is_null($this->publishedAt)) {
             $this->publishedAt = Date::dbNow();
         }
     }
 
     public function isPublished() : bool
     {
-        return $this->parentIsPublished() && Date::happened($this->publishedAt);
+        return $this->parentIsPublished()
+            && $this->publishedAt
+            && Date::happened($this->publishedAt);
     }
 
     public function publishedAtIso() : string
--- src/Models/Traits/Published.php
+++ src/Models/Traits/Published.php
@@ -2,31 +2,11 @@
 
 namespace Plasticode\Models\Traits;
 
-use Plasticode\Query;
-
 /**
  * Limited publish support: only published (no published_at).
  */
 trait Published
 {
-    /**
-     * For Tags trait.
-     */
-    protected static function tagsWhere(Query $query) : Query
-    {
-        return self::wherePublished($query);
-    }
-
-    public static function getPublished() : Query
-    {
-        return self::wherePublished(self::query());
-    }
-
-    protected static function wherePublished(Query $query) : Query
-    {
-        return $query->where('published', 1);
-    }
-
     public function publish()
     {
         $this->published = 1;
