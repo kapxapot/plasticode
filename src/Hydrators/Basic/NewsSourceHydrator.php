d717f8f8527047df975d594988559a926f9eabf6
Sergey Atroshchenko <kapxapot@gmail.com>
2020-06-26 01:58:36 +0300

    NewsSource + Hydrator

---
+++ src/Hydrators/Basic/NewsSourceHydrator.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Plasticode\Hydrators\Basic;
+
+use Plasticode\Core\Interfaces\LinkerInterface;
+use Plasticode\Models\DbModel;
+use Plasticode\Models\NewsSource;
+use Plasticode\Parsing\Interfaces\ParserInterface;
+use Plasticode\Parsing\Parsers\CutParser;
+use Plasticode\Repositories\Interfaces\UserRepositoryInterface;
+
+abstract class NewsSourceHydrator extends ParsingHydrator
+{
+    protected UserRepositoryInterface $userRepository;
+
+    protected CutParser $cutParser;
+    protected LinkerInterface $linker;
+
+    public function __construct(
+        UserRepositoryInterface $userRepository,
+        CutParser $cutParser,
+        LinkerInterface $linker,
+        ParserInterface $parser
+    )
+    {
+        parent::__construct($parser);
+
+        $this->userRepository = $userRepository;
+
+        $this->cutParser = $cutParser;
+        $this->linker = $linker;
+    }
+
+    /**
+     * @param NewsSource $entity
+     */
+    public function hydrate(DbModel $entity) : NewsSource
+    {
+        return $entity
+            ->withParsed(
+                $this->frozen(
+                    fn () => $this->parse($entity->rawText())
+                )
+            )
+            ->withFullText(
+                $this->frozen(
+                    fn () =>
+                    $this->cutParser->full(
+                        $entity->parsedText()
+                    )
+                )
+            )
+            ->withShortText(
+                $this->frozen(
+                    fn () =>
+                    $this->cutParser->short(
+                        $entity->parsedText()
+                    )
+                )
+            )
+            ->withTagLinks(
+                fn () => $this->linker->tagLinks($entity)
+            )
+            ->withCreator(
+                fn () => $this->userRepository->get($entity->createdBy)
+            )
+            ->withUpdater(
+                fn () => $this->userRepository->get($entity->updatedBy)
+            );
+    }
+}
---
+++ src/Models/NewsSource.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Plasticode\Models;
+
+use Plasticode\Collections\TagLinkCollection;
+use Plasticode\Models\DbModel;
+use Plasticode\Models\Interfaces\NewsSourceInterface;
+use Plasticode\Models\Interfaces\SearchableInterface;
+use Plasticode\Models\Traits\FullPublished;
+use Plasticode\Models\Traits\Linkable;
+use Plasticode\Models\Traits\Stamps;
+use Plasticode\Models\Traits\Tagged;
+use Plasticode\Parsing\ParsingContext;
+
+/**
+ * @method ParsingContext parsed()
+ * @method TagLinkCollection tagLinks()
+ * @method static withFullText(string|callable|null $fullText)
+ * @method static withParsed(ParsingContext|callable $parsed)
+ * @method static withShortText(string|callable|null $shortText)
+ * @method static withTagLinks(TagLinkCollection|callable $tagLinks)
+ */
+abstract class NewsSource extends DbModel implements NewsSourceInterface, SearchableInterface
+{
+    use FullPublished;
+    use Linkable;
+    use Stamps;
+    use Tagged;
+
+    protected string $fullTextPropertyName = 'fullText';
+    protected string $parsedPropertyName = 'parsed';
+    protected string $shortTextPropertyName = 'shortText';
+
+    protected function requiredWiths(): array
+    {
+        return [
+            $this->creatorPropertyName,
+            $this->fullTextPropertyName,
+            $this->parsedPropertyName,
+            $this->shortTextPropertyName,
+            $this->tagLinksPropertyName,
+            $this->updaterPropertyName,
+            $this->urlPropertyName,
+        ];
+    }
+
+    public function parsedText() : ?string
+    {
+        return $this->parsed()->text;
+    }
+
+    // NewsSourceInterface
+
+    public function largeImage() : ?string
+    {
+        return $this->parsed()->largeImage();
+    }
+
+    public function image() : ?string
+    {
+        return $this->parsed()->image();
+    }
+
+    public function video() : ?string
+    {
+        return $this->parsed()->video();
+    }
+
+    abstract public function displayTitle() : string;
+
+    abstract public function rawText() : ?string;
+
+    public function hasText() : bool
+    {
+        return strlen($this->rawText()) > 0;
+    }
+
+    public function fullText() : ?string
+    {
+        return $this->getWithProperty(
+            $this->fullTextPropertyName
+        );
+    }
+
+    public function shortText() : ?string
+    {
+        return $this->getWithProperty(
+            $this->shortTextPropertyName
+        );
+    }
+
+    public function creator() : ?User
+    {
+        return $this->getWithProperty(
+            $this->creatorPropertyName
+        );
+    }
+
+    public function tagLinks() : TagLinkCollection
+    {
+        return $this->getWithProperty(
+            $this->tagLinksPropertyName
+        );
+    }
+
+    // LinkableInterface
+    // implemented in Linkable trait
+
+    // TaggedInterface
+    // implemented in Tagged trait
+
+    // SearchableInterface
+
+    abstract public function code() : string;
+}
