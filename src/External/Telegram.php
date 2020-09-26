e853322b62a0b2a1a542597a035a4a60e79293ac
Sergey Atroshchenko <kapxapot@gmail.com>
2020-09-26 13:59:00 +0300

    Whitespace

--- src/External/Telegram.php
+++ src/External/Telegram.php
@@ -22,25 +22,25 @@ class Telegram
     private function curlSendMessage(string $botToken, string $chatId, string $message)
     {
         $url = 'https://api.telegram.org/bot' . $botToken . '/sendMessage';
-    
+
         $params = [
             'chat_id' => '@' . $chatId,
             'text' => $message,
             'parse_mode' => 'html',
         ];
-        
+
         $ch = curl_init();
-        
+
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        
+
         $result = curl_exec($ch);
         curl_close($ch);
-        
+
         return $result;
     }
 }
