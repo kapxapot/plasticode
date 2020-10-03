f72ae7898206ee141a625ed4c6a0e1c7e423c464
Sergey Atroshchenko <kapxapot@gmail.com>
2020-10-03 23:57:23 +0300

    Docblocks and whitespace

--- src/Parsing/Interfaces/TagMapperSourceInterface.php
+++ src/Parsing/Interfaces/TagMapperSourceInterface.php
@@ -6,33 +6,23 @@ interface TagMapperSourceInterface
 {
     /**
      * Registers new mapper for a specified $tag.
-     *
-     * @param string $tag
-     * @param TagMapperInterface $mapper
-     * @return void
      */
-    public function register(string $tag, TagMapperInterface $mapper) : void;
-    
+    function register(string $tag, TagMapperInterface $mapper) : void;
+
     /**
      * Returns registered tags.
      *
      * @return string[]
      */
-    public function getTags() : array;
-    
+    function getTags() : array;
+
     /**
      * Get mapper for the tag. Null if absent.
-     *
-     * @param string $tag
-     * @return TagMapperInterface|null
      */
-    public function getMapper(string $tag) : ?TagMapperInterface;
+    function getMapper(string $tag) : ?TagMapperInterface;
 
     /**
      * Returns component name for $tag. Default = $tag.
-     *
-     * @param string $tag
-     * @return string
      */
-    public function getComponentName(string $tag) : string;
+    function getComponentName(string $tag) : string;
 }
