655f71b8d2fe6fe266eb009c1f9d356d719040a0
Sergey Atroshchenko <kapxapot@gmail.com>
2021-01-22 11:16:44 +0300

    Collection fix #2

--- src/Collections/MappingProviderCollection.php
+++ src/Collections/MappingProviderCollection.php
@@ -7,5 +7,5 @@ use Plasticode\Mapping\Interfaces\MappingProviderInterface;
 
 class MappingProviderCollection extends TypedCollection
 {
-    protected $class = MappingProviderInterface::class;
+    protected string $class = MappingProviderInterface::class;
 }
