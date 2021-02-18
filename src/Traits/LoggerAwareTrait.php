bc701d0b99871675deba56ae8bf3f95ab0a2921d
Sergey Atroshchenko <kapxapot@gmail.com>
2021-02-18 20:10:38 +0300

    Logger logEx

--- src/Traits/LoggerAwareTrait.php
+++ src/Traits/LoggerAwareTrait.php
@@ -2,6 +2,8 @@
 
 namespace Plasticode\Traits;
 
+use Exception;
+use Plasticode\Util\Debug;
 use Psr\Log\LoggerInterface;
 
 trait LoggerAwareTrait
@@ -18,12 +20,21 @@ trait LoggerAwareTrait
         return $this;
     }
 
-    private function log(string $message, ?array $data = null): void
+    protected function log(string $message, ?array $data = null): void
     {
         if (is_null($this->logger)) {
             return;
         }
 
         $this->logger->info($message, $data ?? []);
+    }
+
+    protected function logEx(Exception $ex): void
+    {
+        $this->log(
+            json_encode(
+                Debug::exceptionTrace($ex)
+            )
+        );
     }
 }
