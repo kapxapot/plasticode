0b057945a16e0ca2b6e8db92b5d349f02212fac6
Sergey Atroshchenko <kapxapot@gmail.com>
2021-05-08 23:43:49 +0300

    Extracts EquatableCollection

---
+++ src/Collections/Generic/EquatableCollection.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Plasticode\Collections\Generic;
+
+use Plasticode\Models\Interfaces\EquatableInterface;
+
+class EquatableCollection extends TypedCollection
+{
+    protected string $class = EquatableInterface::class;
+
+    /**
+     * Returns distinct elements using `contains()` function.
+     * 
+     * @return static
+     */
+    public function distinct(): self
+    {
+        $col = static::make();
+
+        /** @var EquatableInterface */
+        foreach ($this as $item) {
+            if ($col->contains($item)) {
+                continue;
+            }
+
+            $col = $col->add($item);
+        }
+
+        return $col;
+    }
+
+    /**
+     * Returns all elements except the specified.
+     * 
+     * @return static
+     */
+    public function except(EquatableInterface ...$elements): self
+    {
+        $toExclude = static::make($elements);
+
+        return $this->where(
+            fn (EquatableInterface $eq) => !$toExclude->contains($eq)
+        );
+    }
+
+    public function contains(?EquatableInterface $element): bool
+    {
+        if (is_null($element)) {
+            return false;
+        }
+
+        return $this->anyFirst(
+            fn (EquatableInterface $eq) => $eq->equals($element)
+        );
+    }
+}
