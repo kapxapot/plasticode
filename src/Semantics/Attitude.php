0739cd4fc3cde5d917e44a548cef172b8d25d81a
Sergey Atroshchenko <kapxapot@gmail.com>
2021-07-30 01:21:19 +0300

    Adds Attitude

---
+++ src/Semantics/Attitude.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Plasticode\Semantics;
+
+/**
+ * Обращение на "вы"/"ты".
+ */
+class Attitude
+{
+    /** Официальное обращение, на "вы". */
+    const OFFICIAL = 1;
+    /** Неофициальное обращение, на "ты". */
+    const UNOFFICIAL = 2;
+}
