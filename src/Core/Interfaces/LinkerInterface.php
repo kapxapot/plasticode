334a666a97553feff9067c1b46b4b8b7fc294f44
Sergey Atroshchenko <kapxapot@gmail.com>
2024-08-28 15:16:30 +0400

    Interface update

--- src/Core/Interfaces/LinkerInterface.php
+++ src/Core/Interfaces/LinkerInterface.php
@@ -8,25 +8,25 @@ use Plasticode\Models\Interfaces\TaggedInterface;
 
 interface LinkerInterface
 {
-    function abs(string $url = null): string;
-    function rel(string $url = null): string;
+    public function abs(string $url = null): string;
+    public function rel(string $url = null): string;
 
-    function getImageExtension(?string $type): string;
+    public function getImageExtension(?string $type): string;
 
-    function page(PageInterface $page = null): string;
-    function news(int $id = null): string;
-    function tag(string $tag = null, string $tab = null): string;
+    public function page(PageInterface $page = null): string;
+    public function news(int $id = null): string;
+    public function tag(string $tag = null, string $tab = null): string;
 
-    function newsYear(int $year): string;
+    public function newsYear(int $year): string;
 
-    function twitch(string $id): string;
-    function twitchImg(string $id): string;
-    function twitchLargeImg(string $id): string;
+    public function twitch(string $id): string;
+    public function twitchImg(string $id): string;
+    public function twitchLargeImg(string $id): string;
 
-    function youtube(string $code): string;
+    public function youtube(string $code): string;
 
-    function defaultGravatarUrl(): string;
-    function gravatarUrl(string $hash = null): string;
+    public function defaultGravatarUrl(): string;
+    public function gravatarUrl(string $hash = null): string;
 
-    function tagLinks(TaggedInterface $entity): TagLinkCollection;
+    public function tagLinks(TaggedInterface $entity): TagLinkCollection;
 }
